c1479eadd27d5eec47cc3fe5929e5faf9d869523
goforpordev <goforprodev@gmail.com>
2022-06-17 00:51:58 +0100

    Added navbar componet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./App.css"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {Box} from "@mui/material"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import ExerciseDetails from "./pages/ExerciseDetails"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Box width={"400px"} sx={{width:{xl:"1488px"}}}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+      <Footer />
+    </Box>
+    </BrowserRouter>
+    // <div>
+    //   #uidshuihs
+    // </div>
   )
 }
 
