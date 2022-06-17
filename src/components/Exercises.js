6eb121580736bc05338f69b574765f59384f8fbb
goforpordev <goforprodev@gmail.com>
2022-06-17 22:39:44 +0100

    Implemented Pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,6 +6,23 @@ import ExerciseCard from './ExerciseCard'
 import {fetchData,exerciseOptions} from '../utils/fetchData'
 
 const Exercises = ({exercises,setexercises,bodyPart}) => {
+  const [currentPage, setcurrentPage] = useState(1)
+  const exercisesPerPage = 9
+
+  // Current Exercise Logic
+  const indexOfLastExercise = currentPage * exercisesPerPage
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+  const currentExercises = exercises.slice(indexOfFirstExercise,indexOfLastExercise)
+
+  
+
+  //Paginate Function
+  function paginate(e,value){
+    setcurrentPage(value)
+
+    window.scrollTo({top:1800,behavior:"smooth"})
+  }
+
   return (
     <Box
       id="exercises"
@@ -17,9 +34,24 @@ const Exercises = ({exercises,setexercises,bodyPart}) => {
     >
       <Typography variant="h3" mb="46px">Showing Results</Typography>
       <Stack direction="row" sx={{gap:{lg:"110px",xs:"50px"}}} flexWrap="wrap" justifyContent="center">
-       {exercises.map((exercise,index) => (
+       {currentExercises.map((exercise,index) => (
           <ExerciseCard key={index} exercise={exercise}/>
        ))} 
+      </Stack>
+      <Stack mt="100px" alignItems={"center"}>
+          {
+            exercises.length > exercisesPerPage ? 
+              <Pagination
+                color='standard'
+                shape='rounded'
+                count={Math.ceil(exercises.length/ exercisesPerPage)}
+                defaultPage={1}
+                size="large"
+                page={currentPage}
+                onChange={paginate}
+               />
+            :""
+          }
       </Stack>
     </Box>
   )
