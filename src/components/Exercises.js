34c23db01f5fad353f091587cf6a4ce9a0c5ffcb
goforpordev <goforprodev@gmail.com>
2022-06-17 22:56:05 +0100

    Implemented search exercises by body part

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -9,12 +9,25 @@ const Exercises = ({exercises,setexercises,bodyPart}) => {
   const [currentPage, setcurrentPage] = useState(1)
   const exercisesPerPage = 9
 
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = []
+
+      if(bodyPart === 'all'){
+          exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises',exerciseOptions)
+      }else{
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,exerciseOptions)
+      }
+        setexercises(exercisesData)
+      }
+
+    fetchExercisesData()
+  }, [bodyPart])
+  
   // Current Exercise Logic
   const indexOfLastExercise = currentPage * exercisesPerPage
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
   const currentExercises = exercises.slice(indexOfFirstExercise,indexOfLastExercise)
-
-  
 
   //Paginate Function
   function paginate(e,value){
